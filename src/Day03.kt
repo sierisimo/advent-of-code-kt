7126e7ca3437db3115f6cdb5e0552fda17b5b686
Sierisimo Sier <sierisimo@gmail.com>
2021-12-06 23:00:13 -0600

    Adding a second part solution, this is really inneficient

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,5 +1,10 @@
 import kotlin.math.pow
 
+/*
+ * I really dislike this solutions, it's merely brute force and just hacks over hacks... if you have any suggestions
+ * feel free to open an issue with hints or ideas.
+ */
+
 data class GammaEpsilon(
     val gamma: String = "",
     val epsilon: String = ""
@@ -12,11 +17,17 @@ fun GammaEpsilon.toDecimals(): Pair<Int, Int> = Pair(
 
 fun String.binaryToDecimal(): Int = foldRight(Pair(0, 0)) { c, acc ->
     Pair(
-        acc.first + 1,
+        acc.first + 1, // Power number
         acc.second +
                 if (c == '0') 0 else 2.0.pow(acc.first).toInt()
     )
 }.second
+
+val Pair<Int, Int>.biggest: Int
+    get() = if (first > second) 0 else 1
+
+val Pair<Int, Int>.smallest: Int
+    get() = if (first <= second) 0 else 1
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -37,10 +48,39 @@ fun main() {
         return gammaEpsilonValues.fold(Int::times)
     }
 
+    fun part2(input: List<String>): Int {
+        fun getZeroOneCountPair(list: List<String>, position: Int): Pair<Int, Int> =
+            list.fold(Pair(0, 0)) { acc, str ->
+                Pair(acc.first + if (str[position] == '0') 1 else 0, acc.second + if (str[position] == '1') 1 else 0)
+            }
+
+        tailrec fun findNumberByCount(
+            l: List<String>,
+            filterBy: (Int, Pair<Int, Int>) -> Boolean,
+            position: Int = 0
+        ): List<String> {
+            if (l.size == 1) return l
+
+            val zeroOneCountPair = getZeroOneCountPair(l, position)
+            return findNumberByCount(
+                l.filter { filterBy(it[position].digitToInt(), zeroOneCountPair) },
+                filterBy,
+                position + 1
+            )
+        }
+
+        val byBiggest = findNumberByCount(input, { d, p -> d == p.biggest }).first().binaryToDecimal()
+        val byLowest = findNumberByCount(input, { d, p -> d == p.smallest }).first().binaryToDecimal()
+
+        return byBiggest * byLowest
+    }
+
 //    test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
+    println(part2(input))
 }
